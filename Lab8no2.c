2172dae441dc2130d2eb067d679e6abd906fc75a
PanyawatFaktim <s6704062612162@email.kmutnb.ac.th>
2026-03-08 16:31:43 +0700

    show number of no.1 stat

--- Lab8no2.c
+++ Lab8no2.c
@@ -5,6 +5,7 @@ char keys[10] = {'D','B','D','C','C','D','A','E','A','D'};
 
 int main() {
     int i;
+    int count;
     char ans[8][10] = {
         {'A','B','A','C','C','D','E','E','A','D'},
         {'D','B','A','B','C','A','E','E','A','D'},
@@ -18,6 +19,8 @@ int main() {
     for(i = 0; i < 8; i++) {
         printf("std %d => %d\n", (i+1), checkscore(ans[i]));
     }
+    count = countNo1Correct(ans,8);
+    printf("Number of students correct in question 1 = %d\n",count);
     return 0;
 }
 int checkscore(char std[]) {
@@ -28,4 +31,16 @@ int checkscore(char std[]) {
             score++;
     }
     return score;
+}
+int countNo1Correct(char ans[][10], int n) {
+
+    int i;
+    int count = 0;
+
+    for(i = 0; i < n; i++) {
+        if(ans[i][0] == keys[0])
+            count++;
+    }
+
+    return count;
 }
